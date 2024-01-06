e7eada29de47e6050eac4823c83718956404395b
Nikhil Nandanwar <nikhilnandanwar429@gmail.com>
2024-01-07 02:13:10 +0530

    03project

---
+++ React/03tailwindProps/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from "./components/Card"
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    username: "Nikhil",
+    age: 19
+  }
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>
+        Tailwind Test 
+      </h1>
+      <Card username="Chai Aur Code" btnText="Click me" />
+      <Card username="Nikhil"/>
+    </>
+  )
+}
+
+export default App
