c36e50cc0f9c9db1e48e39a1d02c6fe74d2300eb
Nikhil Nandanwar <nikhilnandanwar429@gmail.com>
2024-05-12 17:03:44 +0530

    //

--- JavaScript/Functions/scopes.js
+++ JavaScript/Functions/scopes.js
@@ -15,8 +15,36 @@ function one(){
         const website = "youtube"
         console.log(username);
     }
-    console.log(website);
+    // console.log(website);
+    // console.log(this);
     two();
 }
 
-one()
+// one()
+
+// console.log(this);
+
+
+function cahi() {
+    console.log(this);
+}
+
+//  cahi()
+
+
+const chai = () => {
+    let usernaame = "Nikhil"
+    console.log(this.usernaame); 
+}
+// chai()
+
+
+// const addTwo = (num1 , num2) => {
+//     return num1 + num2
+// }
+
+
+// const addTwo = (num1 , num2) => num1 + num2
+const addTwo = (num1 , num2) => (num1 + num2)
+
+console.log(addTwo(3,4));
